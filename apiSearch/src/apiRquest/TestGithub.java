4bd5502d6ea7fdd0474e422fd52478adf66205f1
YYAIYOULING <40688959+YYAIYOULING@users.noreply.github.com>
2018-07-04 15:59:53 +0800

    Update TestGithub.java

--- apiSearch/src/apiRquest/TestGithub.java
+++ apiSearch/src/apiRquest/TestGithub.java
@@ -4,11 +4,7 @@ public class TestGithub {
 
 	public static void main(String[] args) {
 		System.out.println("测试github。。。。。。");
-		System.out.println("测试github1213113。。。。。。");
-		//测试文件
-		//abcddddd
-		System.out.println("再次修改。。。。。。。。");
-		//添加修改日志
+		//743859843754
 	}
 
 }
