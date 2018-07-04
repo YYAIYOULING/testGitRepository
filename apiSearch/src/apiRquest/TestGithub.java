19c9fd6a124b43edbbd1106407f71a9b0a8d4665
wancheng <wancheng@i2finance.net>
2018-07-04 14:51:44 +0800

    再次测试文武兼

--- apiSearch/src/apiRquest/TestGithub.java
+++ apiSearch/src/apiRquest/TestGithub.java
@@ -5,6 +5,7 @@ public class TestGithub {
 	public static void main(String[] args) {
 		System.out.println("测试github。。。。。。");
 		System.out.println("测试github1213113。。。。。。");
+		//测试文件
 		System.out.println("再次修改。。。。。。。。");
 		//添加修改日志
 	}
