82d296c23d1429dbe1572a5efb1ac1e507c11d69
wancheng <wancheng@i2finance.net>
2018-07-04 14:40:40 +0800

    晓得撒非常

--- apiSearch/src/apiRquest/TestGithub.java
+++ apiSearch/src/apiRquest/TestGithub.java
@@ -3,7 +3,10 @@ package apiRquest;
 public class TestGithub {
 
 	public static void main(String[] args) {
+		System.out.println("测试github。。。。。。");
 		System.out.println("测试github1213113。。。。。。");
+
+
 	}
 
 }
